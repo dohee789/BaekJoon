24a812b883516c3807e1459871af74727bcc2230
dohee789 <dojin3773@gmail.com>
2022-10-19 18:26:56 +0900

    구현

---
+++ 시각.py
@@ -0,0 +1,15 @@
+# H를 입력받기(0 <= H <= 23)
+h = int(input())
+
+count = 0
+# 시
+for i in range(h + 1):
+    # 분
+    for j in range(60):
+        # 초
+        for k in range(60):
+            # 매 시각 안에 '3'이 포함되어 있다면 카운트 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
